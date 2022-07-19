fa0f7ae5908aca9096846fe9fbe0c87f76845088
fabriciotex <fabriciortex@gmail.com>
2022-07-19 01:17:57 -0700

    Set GET routes

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ const app = express();
 // set up express listen port
 const port = "5500";
 app.listen(port, () => console.log(`Server running on port ${port}...`));
+
+// enable JSON middleware
+app.use(express.json());
+
+// get all users
+app.route("/users").get((req, res) => res.json(users));
+
+// get user by id
+app.route("/users/:id").get((req, res) => {
+  const userId = req.params.id;
+  const user = users.find((user) => Number(user.id) === Number(userId));
+
+  user ? res.json(user) : res.json(`User not found.`);
+});
